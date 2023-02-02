f0eb99f3a5e56ab53f9eb2f6a08e70ac347a722e
alaajili <alaajili@student.1337.ma>
2023-02-02 23:30:20 +0100

    start handling request

---
+++ Request/request.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Abderrahmane Laajili on 2/2/23.
+//
+
+#include "request.hpp"
+
+void    handle_requests(const std::vector<Server>& servers)
+{
+
+}
+
---
+++ Request/request.hpp
@@ -0,0 +1,21 @@
+//
+// Created by Abderrahmane Laajili on 2/2/23.
+//
+
+#ifndef WEBSERVER_REQUEST_HPP
+#define WEBSERVER_REQUEST_HPP
+
+#include "../ParsingConfig/parser.hpp"
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+
+
+void    handle_requests(const std::vector<Server>& servers);
+
+
+#endif //WEBSERVER_REQUEST_HPP
