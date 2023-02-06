f89a6525b1c248be5309fb95d0e67e9f7b9466cd
alaajili <alaajili@student.1337.ma>
2023-02-06 18:35:55 +0100

    init sockets 2

--- Request/request.cpp
+++ Request/request.cpp
@@ -6,26 +6,33 @@
 
 void    init_sockets(std::vector<Server>& servers)
 {
+    std::map<int, int> port_sock;
 
     for (size_t i = 0; i < servers.size(); i++) {
         struct sockaddr_in  address;
 
-        servers[i].sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-        if (servers[i].sock_fd < 0) {
-            std::cerr << "socket() failed!!" << std::endl;
-            exit(1);
+        if (port_sock.find(servers[i].port) == port_sock.end()) {
+            servers[i].sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+            if (servers[i].sock_fd < 0) {
+                std::cerr << "socket() failed!!" << std::endl;
+                exit(1);
+            }
+            address.sin_family = AF_INET;
+            address.sin_addr.s_addr = INADDR_ANY;
+            address.sin_port = htons(servers[i].port);
+            memset(address.sin_zero, 0, sizeof address.sin_zero);
+            if (bind(servers[i].sock_fd, (struct sockaddr *) &address, sizeof address)) {
+                std::cerr << "bind() failed!!" << std::endl;
+                exit(1);
+            }
+            if (listen(servers[i].sock_fd, 10)) {
+                std::cerr << "listen() failed!!" << std::endl;
+                exit(1);
+            }
+            port_sock[servers[i].port] = servers[i].sock_fd;
         }
-        address.sin_family = AF_INET;
-        address.sin_addr.s_addr = INADDR_ANY;
-        address.sin_port = htons(servers[i].port);
-        memset(address.sin_zero, 0, sizeof address.sin_zero);
-        if (bind(servers[i].sock_fd, (struct sockaddr *)&address, sizeof address)) {
-            std::cerr << "bind() failed!!" << std::endl;
-            exit(1);
-        }
-        if (listen(servers[i].sock_fd, 10)) {
-            std::cerr << "listen() failed!!" << std::endl;
-            exit(1);
+        else {
+            servers[i].sock_fd = port_sock[servers[i].port];
         }
     }
 }
