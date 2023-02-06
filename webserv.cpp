374407e324bc7177cd7eb0ef0df6da016fbbcb97
alaajili <alaajili@student.1337.ma>
2023-02-06 18:35:55 +0100

    init sockets 2

--- webserv.cpp
+++ webserv.cpp
@@ -14,9 +14,6 @@ int main(int ac, char **av)
         return 1;
     }
     servers = parse_conf_file(av[1]);
-    std::cout << servers[0].port << std::endl;
-    std::cout << servers[0].host << std::endl;
-    std::cout << servers[0].server_name << std::endl;
 
     init_sockets(servers);
     for (size_t i = 0; i < servers.size(); i++) {
